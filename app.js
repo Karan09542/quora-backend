0d6c579ef2c71ef50268059836e826b6e85acd60
Karan09542 <Karan09542@gamil.com>
2025-02-01 16:39:38 +0530

    Config CORS

--- app.js
+++ app.js
@@ -32,10 +32,9 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
+    // allowedHeaders: ["Content-Type", "Authorization"], // ✅ allow किए गए headers
   })
 );
-
-app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
