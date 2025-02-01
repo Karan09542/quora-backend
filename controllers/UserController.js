cdc9511b39cb46558337e38ad605d7bf1ce6583b
Karan09542 <Karan09542@gamil.com>
2025-02-01 13:40:09 +0530

    सीताराम सीताराम हर हर महादेव

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -32,8 +32,8 @@ async function signRefreshToken(userId) {
 function setCookies(res, token) {
   res.cookie("refreshToken", token, {
     httpOnly: true,
-    secure: isSecure,
-    sameSite: "None",
+    secure: true,
+    sameSite: "none",
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
   });
 }
