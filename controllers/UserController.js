66caa1f1b9f8b5a2c7a651d60d94a0b18c0f263d
Karan09542 <Karan09542@gamil.com>
2025-02-01 16:42:19 +0530

    हर हर महादेव

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -32,7 +32,7 @@ async function signRefreshToken(userId) {
 function setCookies(res, token) {
   res.cookie("refreshToken", token, {
     httpOnly: true,
-    secure: isSecure,
+    secure: true,
     sameSite: "none",
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
   });
